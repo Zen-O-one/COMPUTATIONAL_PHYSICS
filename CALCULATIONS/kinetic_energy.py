98e2cfa8d5fe521f9ae235b9e019e3b4f1665fae
Zeno One <zeno-one@example.com>
2026-01-02 17:14:38 +0100

    Re-evaluated code to follow PEP 8 Guidelines

--- CALCULATIONS/kinetic_energy.py
+++ CALCULATIONS/kinetic_energy.py
@@ -1,46 +1,48 @@
 from __future__ import annotations
 
 def kinetic_energy(mass: float,  velocity: float) -> float:
-    """ Returns the kinetic energy in joules.
-
+    """Return the kinetic energy in joules.
     Args:
         mass: mass in kilograms (cannot be negative)
-        velocity: velocity in metres per second. """
+        velocity: velocity in metres per second.
+        Raises: ValueError if mass is negative """
 
     if mass < 0:
-        raise ValueError("Mass must not be Negative")
-    return 0.5*mass*(velocity**2)
+        raise ValueError("Mass must not be negative")
+    return 0.5 * mass * (velocity**2)
 
 
-def prompt_non_negative_float(prompt: str) ->float:
+def prompt_non_negative_float(prompt: str) -> float:
     """Prompts the user until we get a non-negative float """
     while True:
         try:
             value = float(input(prompt).strip())
         except ValueError:
-            print('Please enter Numerical value')
+            print('Please enter numeric value')
             continue
 
         if value < 0:
-            print('Enter a non_negative Value')
+            print('Enter a non-negative value')
             continue
         return value
 
 
 def main() -> None:
-    # try:
-    #     mass = prompt_non_negative_float("Input the mass of the body in Kilogram(kg): ")
-    #     velocity = prompt_non_negative_float('Input the velocity of the body in metre per sec (m/s): ')
-    # except KeyboardInterrupt:
-    #     print("Aborted!!!")
-    mass = prompt_non_negative_float(\
-        'Input the mass of the body in kilogram: ')
-
-    velocity = prompt_non_negative_float(\
-        'Input the velocity of the body in m/s: ')
+    try:
+        mass = prompt_non_negative_float(
+            "Input the mass of the body in Kilogram(kg): "
+        )
+        velocity = prompt_non_negative_float(
+            'Input the velocity of the body in metre per sec (m/s): '
+            )
+    except KeyboardInterrupt:
+        print("Aborted!!!")
 
     ke = kinetic_energy(mass, velocity)
-    print(f'A body of mass {mass:.2f}kg and {velocity:.2f}m/s possess kinetic Energy of {ke:.2f}Joules')
+    print(f
+    'A body of mass {mass:.2f}kg and {velocity:.2f}m/s\
+     has kinetic energy of {ke:.2f}J.'
+    )
 
 
 if __name__ == "__main__":
